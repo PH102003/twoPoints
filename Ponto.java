42c34d48485610aef0c132ba93b46cff3959a8e0
PH102003 <100202317+PH102003@users.noreply.github.com>
2023-03-10 21:07:12 -0300

    Update Ponto.java

--- Ponto.java
+++ Ponto.java
@@ -10,7 +10,7 @@ public class Ponto {
     }
     //método
     public double equaçao(Ponto segundoPonto){
-        //utilização de um outro parâmetro para servir de "auxiliar" no cálculo da equação, atribuindo um outro valor além do de this.x e this.y
+        //utilização de um outro parâmetro para servir de "auxiliar"(segundoPonto) no cálculo da equação, atribuindo um outro valor além do de this.x e this.y
         
         double resultado = Math.sqrt(Math.pow((segundoPonto.x -this.x), 2) + Math.pow((segundoPonto.y - this.y), 2));
         //raiz quadrada ( de (xb - xa)^2 + (yb-ya)^2) ;
