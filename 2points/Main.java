976fbfa33b85ad78fe894aa37afabbd18107101d
PH102003 <100202317+PH102003@users.noreply.github.com>
2023-03-10 21:05:59 -0300

    Update Main.java

--- 2points/Main.java
+++ 2points/Main.java
@@ -3,6 +3,7 @@ public class Main{
         Ponto p1= new Ponto(1,1);
         Ponto p2= new Ponto(5,4);
         double distanciaentre2pontos = p1.equaçao(p2);
-        System.out.println(distanciaentre2pontos);
+        System.out.println("Seu resultado é:"+ distanciaentre2pontos);
+    
     }
 }
